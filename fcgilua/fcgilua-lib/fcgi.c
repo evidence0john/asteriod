2febf386aa099e6313a820587e08f33ff00258db
evidence0john <evidence_john@outlook.com>
2017-02-15 21:31:00 +0800

    Wed Feb 15

--- fcgilua/fcgilua-lib/fcgi.c
+++ fcgilua/fcgilua-lib/fcgi.c
@@ -6,6 +6,14 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
+#define ENCRYPT_GAP 10
+
+#define ERR_POST "ERR_POST"
+#define ERR_ALLOC "ERR_ALLOC"
+#define ERR_FILE_POST -21
+#define ERR_FILE_BUF -22
+#define ERR_FILE_OPEN -23
+
 static long counter = 0;
 
 int _HashKey(char *key, unsigned int seed)
@@ -52,16 +60,78 @@ int tpost(lua_State *L)
 	int len;
 	if(sscanf(getenv("CONTENT_LENGTH"), "%d", &len) != 1)
 		len = 0;
-	if(!len) return 1;
+	if(!len){
+		lua_pushstring(L, ERR_POST);
+		return 1;
+	}
 	buffer = (char *)malloc(len + 1);
 	if(buffer == NULL){
-		lua_pushinteger(L, len);
+		lua_pushstring(L, ERR_ALLOC);
 		return 1;
 	}
 	fread(buffer, len, 1, stdin);
 	buffer[len] = 0;
 	lua_pushstring(L, buffer);
 	free(buffer);
+	return 1;
+}
+
+int _wfile(lua_State *L) //post single file
+{
+	char *buffer, *fstart, *fend;
+	char *filename = (char*)lua_tostring(L, 1);
+	char *mod = (char*)lua_tostring(L, 2);
+	int len;
+	FILE *fp;
+	if(sscanf(getenv("CONTENT_LENGTH"), "%d", &len) != 1)
+		len = 0;
+	if(!len){
+		lua_pushinteger(L, ERR_FILE_POST);
+		return 1;
+	}
+	buffer = (char *)malloc(len);
+	if(buffer == NULL){
+		lua_pushinteger(L, ERR_FILE_BUF);
+		return 1;
+	}
+	fread(buffer, len, 1, stdin);
+	fstart = buffer;
+	fend = buffer + len;
+	fend -= ENCRYPT_GAP;
+find_start:
+	while(*fstart++ != 10){
+		if(fstart > fend){
+			lua_pushinteger(L, ERR_FILE_POST);
+			free(buffer);
+			return 1;
+		}
+	}
+	fstart++;
+	if (*fstart != 10 && *fstart !=13) goto find_start;
+	fstart++;
+	if(fstart > fend){
+		lua_pushinteger(L, ERR_FILE_POST);
+		free(buffer);
+		return 1;
+	}
+	while(*fend-- != 10){
+		if(fstart > fend){
+			lua_pushinteger(L, ERR_FILE_POST);
+			free(buffer);
+			return 1;
+		}
+	}
+	len = (fend - fstart);
+	if(mod == NULL) mod = "wb+";
+	fp = fopen(filename, mod);
+	if(fp == NULL){
+		lua_pushinteger(L, ERR_FILE_OPEN);
+		return 1;
+	}
+	fwrite(fstart, len, 1, fp);
+	fclose(fp);
+	free(buffer);
+	lua_pushinteger(L, len);
 	return 1;
 }
 
@@ -100,6 +170,7 @@ static luaL_Reg libs[] = {
 	{"query", query},
 	{"echo", _echo},
 	{"tpost", tpost},
+	{"file", _wfile},
 	{"getip", getip},
 	{"env", _env},
 	{"set_counter", set_counter},
